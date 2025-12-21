d585c4f53319e658790ecb7d0c44aae7bae10cdb
najijrwan <najijrwanyt@gmail.com>
2025-12-21 20:01:09 +0200

    .

--- src/components/sections/LatestProducts.jsx
+++ src/components/sections/LatestProducts.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
+const list = 
+
 const LatestProducts = ({ title }) => {
     return (
-        <section>
-            <h1>
-                { title }
-            </h1>
+        <section 
+            className='
+                flex flex-col items-center'
+        >
+            <header className='flex flex-col items-start gap-[19px]'>
+                <h1 className='caption-title text-text'>
+                    {title}
+                </h1>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </header>
+            <main>
+
+            </main>
         </section>
     );
 }
