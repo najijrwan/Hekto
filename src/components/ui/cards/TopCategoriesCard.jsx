d45327d84b3d001b3530fe907dfcc49917a69dc6
najijrwan <najijrwanyt@gmail.com>
2025-12-27 22:39:16 +0200

    .

--- src/components/ui/cards/TopCategoriesCard.jsx
+++ src/components/ui/cards/TopCategoriesCard.jsx
@@ -4,20 +4,30 @@ const TopCategoriesCard = ({ product }) => {
     return (
         <div
             className="
+            relative
             w-[270px] h-[345px]
-            flex flex-col justify-between items-center"
+            flex flex-col justify-between items-center
+            group"
         >
             <div
                 className="
                 size-[270px] rounded-full
                 flex items-center justify-center
-                bg-[#F6F7FB] shadow-2"
+                bg-[#F6F7FB] shadow-2 z-20"
             >
                 <img
                     src={product.image}
                     alt="Chair"
                     className="size-[160px]"
                 />
+            </div>
+
+            <div
+                className="
+                absolute top-[6px] right-[5px] 
+                size-[270px] rounded-full 
+                bg-[#9877E7] opacity-0 group-hover:opacity-100 transition-opacity duration-200 ease-out z-10"
+            >
             </div>
 
             <div
